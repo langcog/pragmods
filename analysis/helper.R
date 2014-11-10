f614631165f246e7051ff6e5302d7f97e6415791
Michael C. Frank <mcfrank@stanford.edu>
2014-11-09 17:23:29 -0800

    updates with size and sequences

--- analysis/helper.R
+++ analysis/helper.R
@@ -1,6 +1,11 @@
 read.turk <- function (fname) {
-  d <- read.table(fname,
-                  header=TRUE, sep="\t", row.names=NULL, stringsAsFactors = FALSE)
+  if (grepl("csv", fname)) {
+    d <- read.table(fname,
+                    header=TRUE, sep=",", row.names=NULL, stringsAsFactors = FALSE)
+    } else {
+    d <- read.table(fname,
+                    header=TRUE, sep="\t", row.names=NULL, stringsAsFactors = FALSE)
+  }
   names(d) <- str_replace(tolower(names(d)),"answer.","")
   d <- d %>%
     select(-hitid, -hittypeid, -title, -description, -keywords,
@@ -13,8 +18,7 @@ read.turk <- function (fname) {
     select(-about, -comment,          
            -familiarization_present_in_study,
            -participant_feature_count_condition, 
-           -target_filler_sequence_condition, 
-           -linguistic_framing_condition, 
+           -target_filler_sequence_condition,            
            -question_type_condition) # substantive 
   
   types <- sapply(d, class)
@@ -24,18 +28,24 @@ read.turk <- function (fname) {
       d[,names(types[i])] <- str_replace_all(d[,names(types[i])], 
                                                           "\"", "")
     }
+    
+    if (grepl("choice_correct_", names(types[i]))) {
+      d[,names(types[i])] <- as.logical(d[,names(types[i])])
+    }
   }
+  
+
   
   d <- d %>% rename(time = assignmentsubmittime, 
                     targ.prop = target_property,
                     familiarization = familiarization_cond, 
                     response.cond = participant_response_type_condition,
+                    ling.cond = linguistic_framing_condition,
                     targ.bet = money_allocated_to_target, 
                     dist.bet = money_allocated_to_logical,
                     foil.bet = money_allocated_to_foil,                    
                     mc.targ = manip_check_target, 
-                    mc.dist = manip_check_dist, 
-                    targ.pos = target_position, 
+                    mc.dist = manip_check_dist,                     
                     mc.name = name_check_correct, 
                     targ.likert = likert_value_target,
                     dist.likert = likert_value_foil,
